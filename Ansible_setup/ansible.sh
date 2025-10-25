6bae453e372d2e29cb97a4ea962267b4ab745fb0
Shashwat Prasad <shashwat.prasad.telstra@gmail.com>
2025-10-25 21:14:03 +1100

    test

--- Ansible_setup/ansible.sh
+++ Ansible_setup/ansible.sh
@@ -11,3 +11,11 @@ pwd
 # Start the Ansible container in the background
 docker-compose up -d
 
+
+mkdir -p /root/Git_Clone/ubuntu_setup/Ansible_setup/package
+cd /root/Git_Clone/ubuntu_setup/Ansible_setup/package
+
+# Download curl and its dependencies for Alpine (example for Alpine 3.18)
+apk fetch --repository=http://dl-cdn.alpinelinux.org/alpine/v3.18/main curl
+apk fetch --repository=http://dl-cdn.alpinelinux.org/alpine/v3.18/main libcurl
+
